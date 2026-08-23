1d13a286f44ec8559e9efbea5d7c38929edf4e50
itsmehengg <ngweiheng15@gmail.com>
2026-08-23 21:14:23 +0800

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://zadpajrsmgjuniryqcqp.supabase.co',
+    publishableKey: 'sb_publishable_3AUx7p1OFYZSe4Hvxifr_g_9I0cQfYR',
+  );
+
   runApp(const GoTransitApp());
 }
