71a78d48001794afca9e703670bcae9a9daf0eaf
Yw206 <165979900+Yw206@users.noreply.github.com>
2026-08-25 23:37:46 +0800

    Initialise persistent Module 1 settings

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
+import 'features/profile/personalisation_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,8 @@ Future<void> main() async {
     url: 'https://zadpajrsmgjuniryqcqp.supabase.co',
     publishableKey: 'sb_publishable_3AUx7p1OFYZSe4Hvxifr_g_9I0cQfYR',
   );
+
+  await PersonalisationService.instance.initialise();
 
   runApp(const GoTransitApp());
 }
