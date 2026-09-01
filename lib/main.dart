e0fdbca5ca371d15769153edb4d24dede695d2bc
Yw206 <165979900+Yw206@users.noreply.github.com>
2026-09-02 02:16:51 +0800

    Add service alert push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
+import 'features/alerts/notification_service.dart';
 import 'features/profile/personalisation_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
   await Supabase.initialize(
     url: 'https://zadpajrsmgjuniryqcqp.supabase.co',
@@ -13,6 +18,9 @@ Future<void> main() async {
   );
 
   await PersonalisationService.instance.initialise();
+  await NotificationService.instance.initialise(
+    enabled: PersonalisationService.instance.notificationsEnabled,
+  );
 
   runApp(const GoTransitApp());
 }
